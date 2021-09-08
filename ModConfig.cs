021e7d900f44330a3062f395214ce0b804fd13d7
joisse1101 <thoiwei@gmail.com>
2021-09-09 00:26:15 +0800

    Initial commit

---
+++ ModConfig.cs
@@ -0,0 +1,23 @@
+﻿using StardewModdingAPI;
+
+namespace InstantAnimals
+{
+    public class ModConfig
+    {
+        /// <summary>
+        /// Indicates if animals should cost their usual resources.
+        /// </summary>
+        public bool BuyUsesResources { get; set; } = false;
+
+        /// <summary>
+        /// Indicates if animals are sold as adults or babies.
+        /// </summary>
+        public bool BuyAdultLivestock { get; set; } = true;
+
+        /// <summary>
+        /// Button to open and close the Instant Animals menu
+        /// </summary>
+        public SButton ToggleInstantBuyMenuButton { get; set; } = SButton.J;
+
+    }
+}
